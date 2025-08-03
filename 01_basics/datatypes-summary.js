f592114425c800edd6a833d7502b9f6dd27f9b43
Ananya Singh <ananyasingh20635@gmail.com>
2025-08-03 22:10:23 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,6 @@
+// Primitive(copy created) -> no changes in original
+// Non-primitive (reference) -> same changes in original
+
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
@@ -6,21 +9,23 @@ const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const outsideTemp = null // null means absence of anything
+let userEmail; //undefined by default 
 
+//Symbols are used for unique identification
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId); // always false
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n  // add n to convert to bigint
 
 
 
 // Reference (Non primitive)
 
-// Array, Objects, Functions
+//       Array, Objects, Functions
+// type: object , object , object function
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
@@ -32,6 +37,6 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof anotherId); //Symbol
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
