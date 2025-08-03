2d9c920529496caa2e072fae90028dbef9b6271e
Ananya Singh <ananyasingh20635@gmail.com>
2025-08-03 21:35:17 +0530

    IIFE
    if ; missing then why 2 IIFE shows error?

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -4,9 +4,11 @@
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
-
+})(); 
+// if ; missing =>  JS misinterprets the second IIFE as an argument being passed to the result of the first function.
+// ResultOfIIFE1 (function aurcode() {...})();
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
+
 
