2cd824500c55f3959ecf7ede50b5c2d8d69686ee
Ananya Singh <ananyasingh20635@gmail.com>
2025-08-03 21:46:30 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,22 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
+// console.log("2" > 1); // implicitly convert
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// not recommended 
+  console.log(null > 0);
+  console.log(null == 0); 
+  console.log(null >= 0);
+  // == equality operator does not implicitly convert null to 0 
+  // comparison operators implicitly convert null to 0
+  
+  console.log(undefined == 0);
+  console.log(undefined > 0);
+  console.log(undefined < 0);
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); 
+// strict equality === operator checks both value and datatype
+// loose equality == operator checks only value
